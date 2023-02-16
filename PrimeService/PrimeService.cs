4f61e72cc409c3db61f3e85a73b3629f83229f32
anhtrungg <anhtrung.work@gmail.com>
2023-02-16 21:00:25 +0700

    Init unit test

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeService
+{
+    public class PrimeService
+    {
+        
+        public bool IsPrime(int candidate)
+        {
+            if(candidate < 2)
+            {
+                return false;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
